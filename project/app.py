52af96230b7ef62dd530d95a224d5a463a2ce092
Sarah John <sarahjohn678@gmail.com>
2024-09-30 22:44:50 -0400

    passes 5 tests

--- project/app.py
+++ project/app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, g, render_template, request, session, flash, redirect, url_for
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort
 
 # configuration
 DATABASE = "flaskr.db"
@@ -43,6 +43,20 @@ def logout():
     """User logout/authentication/session management."""
     session.pop('logged_in', None)
     flash('You were logged out')
+    return redirect(url_for('index'))
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Add new post to database."""
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute(
+        'insert into entries (title, text) values (?, ?)',
+        [request.form['title'], request.form['text']]
+    )
+    db.commit()
+    flash('New entry was successfully posted')
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
